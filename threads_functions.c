6ad825e3f08b34d9862aef0030dbdcd6fd34c8a6
mohamedamine456 <aminelachheb456@gmail.com>
2021-07-14 14:37:25 +0100

    segfault fixed

--- threads_functions.c
+++ threads_functions.c
@@ -6,11 +6,19 @@
 /*   By: mlachheb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 15:55:43 by mlachheb          #+#    #+#             */
-/*   Updated: 2021/07/12 20:46:42 by mlachheb         ###   ########.fr       */
+/*   Updated: 2021/07/14 14:36:43 by mlachheb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
+
+void	create_philosopher_thread(t_philosopher *philo)
+{
+	usleep(50);
+	if (pthread_create(&(philo->philo_thread), NULL,
+			philosopher_life, philo) != 0)
+		fatal("create philosophers thread failed!");
+}
 
 void	create_threads(t_philosopher *philos, int number_philos)
 {
@@ -19,18 +27,17 @@ void	create_threads(t_philosopher *philos, int number_philos)
 	t_supervisor	supervisor;
 
 	i = 0;
+	if (philos == NULL)
+		return ;
 	tmp = philos;
 	supervisor.philosophers = philos;
 	supervisor.write_mutex = philos->write_mutex;
 	if (pthread_create(&(supervisor.superv_thread), NULL,
 			supervisor_life, &supervisor) != 0)
 		fatal("create supervisor thread failed!");
-	while (i < number_philos)
+	while (i < number_philos && tmp != NULL)
 	{
-		usleep(50);
-		if (pthread_create(&(tmp->philo_thread), NULL,
-				philosopher_life, tmp) != 0)
-			fatal("create philosophers thread failed!");
+		create_philosopher_thread(tmp);
 		i++;
 		tmp = tmp->next;
 	}	
